5ccc2afb8ff3d39d39e4faa268e27a453e57634a
Gilbert-Silva <gilbertif1995@outlook.com>
2024-05-22 15:37:07 -0300

    Crud Cliente

--- treinamento/agenda/urls.py
+++ treinamento/agenda/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import index, listar_clientes, inserir_cliente, editar_cliente, excluir_cliente
+from .views import index, cliente_listar, cliente_inserir, cliente_editar, cliente_excluir
 
 urlpatterns = [
     path('', index, name='index'),
-    path('clientes/', listar_clientes, name='listar_clientes'),
-    path('clientes/inserir/', inserir_cliente, name='inserir_cliente'),
-    path('clientes/editar/<int:cliente_id>/', editar_cliente, name='editar_cliente'),
-    path('clientes/excluir/<int:cliente_id>', excluir_cliente, name='excluir_cliente'),
+    path('clientes/', cliente_listar, name='cliente_listar'),
+    path('clientes/inserir/', cliente_inserir, name='cliente_inserir'),
+    path('clientes/editar/<int:cliente_id>/', cliente_editar, name='cliente_editar'),
+    path('clientes/excluir/<int:cliente_id>/', cliente_excluir, name='cliente_excluir'),
 ]
 
 
--- treinamento/agenda/views.py
+++ treinamento/agenda/views.py
@@ -8,39 +8,48 @@ from agenda.forms import ClienteForm
 def index(request):
     return render(request, 'index.html')
 
-def listar_clientes(request):
+def cliente_listar(request):
     clientes = Cliente.objects.all()
-    return render(request, 'listar_clientes.html', {'clientes': clientes})
+    return render(request, 'cliente_listar.html', {'clientes': clientes})
 
-def inserir_cliente(request):
+def cliente_inserir(request):
     #invalido = False
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_clientes')
+            return redirect('cliente_listar')
         else:
             messages.error(request, "Informe os dados corretamente!")
             #invalido = True
-            return redirect('inserir_cliente')
+            return redirect('cliente_inserir')
     else:
         form = ClienteForm()
     #return render(request, 'inserir_cliente.html', {'form': form, 'invalido' : invalido })
-    return render(request, 'inserir_cliente.html', {'form': form })
+    return render(request, 'cliente_inserir.html', {'form': form })
 
-def editar_cliente(request, cliente_id):
+def cliente_editar(request, cliente_id):
     cliente = get_object_or_404(Cliente, pk=cliente_id)
     if request.method == 'POST':
         form = ClienteForm(request.POST, instance=cliente)
         if form.is_valid():
             form.save()
-            return redirect('listar_clientes')
+            return redirect('cliente_listar')
+        else:
+            messages.error(request, "Informe os dados corretamente!")
+            return redirect('cliente_editar')
     else:
         form = ClienteForm(instance=cliente)
-    return render(request, 'editar_cliente.html', {'form': form})
+    return render(request, 'cliente_editar.html', {'form': form})
 
-def excluir_cliente(request, cliente_id):
+def cliente_excluir(request, cliente_id):
+    cliente = get_object_or_404(Cliente, pk=cliente_id)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('cliente_listar')
+    return render(request, 'cliente_excluir.html', {'cliente': cliente})
+    
     cliente = get_object_or_404(Cliente, pk=cliente_id)
     # Lógica para remover o cliente
     cliente.delete()
-    return redirect('listar_clientes')
+    return redirect('cliente_listar')
