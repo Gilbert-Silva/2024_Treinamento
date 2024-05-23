d8213e9965fdea7ac8e1e702bf9a7a6411c6b592
Gilbert-Silva <gilbertif1995@outlook.com>
2024-05-23 08:20:32 -0300

    Servicos

--- treinamento/agenda/admin.py
+++ treinamento/agenda/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Servico
 
 # Register your models here.
 
 admin.site.register(Cliente)
+admin.site.register(Servico)
+
 
--- treinamento/agenda/models.py
+++ treinamento/agenda/models.py
@@ -9,3 +9,12 @@ class Cliente(models.Model):
 
     def __str__(self):
         return f"{self.nome} - {self.email} - {self.fone}"
+
+class Servico(models.Model):
+    descricao = models.CharField(max_length=100)
+    valor = models.FloatField()
+    duracao = models.IntegerField()  # Duração em minutos, por exemplo
+
+    def __str__(self):
+        return f"{self.descricao} - {self.valor:.2f} - {self.duracao}"
+
